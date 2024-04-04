3772fa688ee6201dc9955ead6c8b6f5e77b672e0
yunseo <seoyungyo@seoyungyoui-MacBookAir.local>
2024-04-04 19:15:03 +0900

    add toString()

--- Process.java
+++ Process.java
@@ -1,12 +1,26 @@
-public class Process{
+public class Process {
     private int pid;
     private int priority;
     private int arrive_time;
     private int response_time;
     private int finish_time;
 
-    public Process()
-    {
+    public Process(int pid, int priority, int arrive_time, int response_time, int finish_time) {
+        this.pid = pid;
+        this.priority = priority;
+        this.arrive_time = arrive_time;
+        this.response_time = response_time;
+        this.finish_time = finish_time;
+    }
 
+    public int getPid() { return pid; }
+    public int getPriority() { return priority; }
+    public int getArrive_time() { return arrive_time; }
+    public int getResponse_time() { return response_time; }
+    public int getFinish_time() { return finish_time; }
+
+    @Override
+    public String toString() {
+        return pid + ", " + priority + ", " + arrive_time + ", " + response_time + ", " + finish_time;
     }
 }
