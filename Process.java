0e7e9668e761a5a1791c354e06c76c0b62bbb66d
yunseo <seoyungyo@seoyungyoui-MacBookAir.local>
2024-04-04 18:26:51 +0900

    first commit in intellij

---
+++ Process.java
@@ -0,0 +1,12 @@
+public class Process{
+    private int pid;
+    private int priority;
+    private int arrive_time;
+    private int response_time;
+    private int finish_time;
+
+    public Process()
+    {
+
+    }
+}
