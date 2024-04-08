1f1b277c0973ed402f59bd6d059ee0e7bd87c5a7
ParkJiwon <parkjiwon0821@naver.com>
2024-04-08 14:06:20 +0900

    윤교바보

--- scheduler_gui/JYScheduler.java
+++ scheduler_gui/JYScheduler.java
@@ -8,11 +8,15 @@ import java.awt.event.*;
 public class JYScheduler extends JFrame{
 
     public JYScheduler(){
-        setTitle("DeliciousCoffee"); // 창타이틀 설정
+        setTitle("JYScheduler"); // 창타이틀 설정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container frame = getContentPane(); // frame 컨테이너 생성
+        frame.setLayout(new BorderLayout()); // borderlayout으로
 
-        setSize(500, 500); // 창 사이즈 설정
+        JLabel welcome = new JLabel("윤교바보"); // 라벨 생성
+        frame.add(welcome, BorderLayout.NORTH); // welcomflowFrame은 North로
+
+        setSize(700, 800); // 창 사이즈 설정
         setLocationRelativeTo(null);// JFrame을 화면 중앙에 위치시킴
         setVisible(true);
 
