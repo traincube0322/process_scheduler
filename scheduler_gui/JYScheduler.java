74981a416ecb9098fe4e845164fa72360f1396ef
ParkJiwon <parkjiwon0821@naver.com>
2024-04-08 13:31:05 +0900

    창 생성

--- scheduler_gui/JYScheduler.java
+++ scheduler_gui/JYScheduler.java
@@ -1,7 +1,23 @@
+// jiwon_gui 브랜치에서 커밋하기!!!
+
 package scheduler_gui;
 
-public class JYScheduler {
-    public static void main(String[] args){
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class JYScheduler extends JFrame{
 
+    public JYScheduler(){
+        setTitle("DeliciousCoffee"); // 창타이틀 설정
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container frame = getContentPane(); // frame 컨테이너 생성
+
+        setSize(500, 500); // 창 사이즈 설정
+        setLocationRelativeTo(null);// JFrame을 화면 중앙에 위치시킴
+        setVisible(true);
+
+    }
+    public static void main(String[] args){
+        JYScheduler schedulerFrame = new JYScheduler();
     }
 }
