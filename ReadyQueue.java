8ca40ed3259fe1f0fb260ecb1f69352f192c422e
yunseo <seoyungyo@seoyungyoui-MacBookAir.local>
2024-04-09 14:57:25 +0900

    modified ReadyQueue

--- ReadyQueue.java
+++ ReadyQueue.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class ReadyQueue {
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReadyQueue {
+
+    Queue <Process> Q;
+
+    public ReadyQueue() { Q = new LinkedList<>(); }
+    public void enqueue(Process p) { Q.offer(p); }
+    public Process dequeue() { return Q.poll(); }
+    public boolean isEmpty() { return Q.isEmpty(); }
+    public int size() { return Q.size(); }
+
+    public void displayQueue ()
+    {
+        System.out.println("Ready Queue");
+        for (Process p : Q)
+            System.out.println(p);
+    }
+
+
 }
