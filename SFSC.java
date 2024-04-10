9f5191ff32a1a279b0ce4f53191110e714a4aa4f
yunseo <yunseo@DESKTOP-LRNM1MV>
2024-04-10 13:12:38 +0900

    SFSC edit

--- SFSC.java
+++ SFSC.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class SFSC {
 
     private int time;
@@ -10,19 +12,24 @@ public class SFSC {
     }
 
     public void run() {
-        pp.displayProcessPoll();
-        rq.displayQueue();
         int elapsedTime = 0;
         Process runningProcess = null;
-        // 프로세스 풀에 없을때 까지, Q에 없을때 까지
-        while (!pp.isEmpty() || !rq.isEmpty()) {
+        // 프로세스 풀에 없고, READY QUEUE가 비고, RUNNING PROCESS가 없다면 종료
+        while (runningProcess != null || (!pp.isEmpty() || !rq.isEmpty())) {
             System.out.println("time : " + time);
-            rq.displayQueue();
-            // 이 부분 first 말고 다른 프로세스도 arrive타임인지 확인해야함.
-            // arriveTime 기준 min Heap 쓰는게 더 좋을듯?
+
+            // 도착 시간이 된 프로세스는 PP에서 RQ로 옮김.
             while (!pp.isEmpty() && time == pp.peek().getArriveTime()) {
                 rq.enqueue(pp.poll());
             }
+            //pp.displayProcessPoll();
+            //rq.displayQueue();
+            // running 프로세스가 있고, 프로세스가 시간을 다 썼다면?
+            if (runningProcess != null && elapsedTime == runningProcess.getBurstTime()) {
+                System.out.println("pid : " + runningProcess.getPid() + " end at " + time);
+                runningProcess = null;
+            }
+            // 현재 실행중인 프로세스가 없다면?
             if (runningProcess == null) {
                 if (!rq.isEmpty()) {
                     runningProcess = rq.dequeue();
@@ -30,11 +37,9 @@ public class SFSC {
                     elapsedTime = 0;
                 }
             }
-            else elapsedTime++;
-            if (!(runningProcess == null) && elapsedTime == runningProcess.getBurstTime()) {
-                System.out.println("pid : " + runningProcess.getPid() + " end at " + time);
-                runningProcess = null;
-            }
+            // 프로세스 있으면 시간 증가.
+            elapsedTime++;
+
             // RQ 에서 프로세스 하나를 뽑아서 CPU Burst 시킴. : Running Process
             // 그 프로세스의 elapsed time == brust time 이면 RQ에서 새 프로세스 다시 뽑기.
 
